324e958ee57baf0b2da1ad3fe8771bd35b23bab6
mkcomru <grebenshchikov.my@students.dvfu.ru>
2025-03-22 10:23:30 +1000

    made shema for users

---
+++ backend/app/models/user.py
@@ -0,0 +1,14 @@
+from tortoise import Tortoise, fields, models, run_async
+
+
+class User(models.Model):
+    id = fields.IntField(primary_key=True)
+    telegram_id = fields.IntField(unique=True)
+    first_name = fields.CharField(max_length=255)
+    last_name = fields.CharField(max_length=255)
+    username = fields.CharField(max_length=255)
+    birthday = fields.DateField()
+
+    class Meta:
+        table = "users"
+
